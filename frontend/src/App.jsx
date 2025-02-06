a3e5d9139447579d05f822891bdf9318a1c17433
mahilmithranks <mahilmithranks2007@gmail.com>
2025-02-06 16:35:42 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Home from './pages/home';
 import Login from './pages/login';
 import Navbar from './pages/Navbar';
+import Signup
+ from './pages/signup';
 function App() {
   return (
      <BrowserRouter>
@@ -10,6 +12,7 @@ function App() {
        <Routes>
             <Route  path="/"  element={<Home/>}/>
             <Route path="/login" element={<Login/>}/>
+            <Route path="/signup" element={<Signup/>}/>
 
        </Routes>
      </BrowserRouter>

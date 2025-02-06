7f2830dd66b14884f776812a05ea995bb347a341
mahilmithranks <mahilmithranks2007@gmail.com>
2025-02-06 10:04:28 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,9 @@ const app=express()
 app.use(express.json())
 
 
-const{catchAsyncError} =require("./middleware/catchAsyncError")
-const {ErrorHandler} =require("./utils/errorHandler")
-const errMiddleware =require("./middleware/error")
+const{catchAsyncError} =require("../middleware/catchAsyncError")
+const {ErrorHandler} =require("../utils/errorHandler")
+const errMiddleware =require("../middleware/error")
 
 app.use(errMiddleware)
 module.exports={app}
